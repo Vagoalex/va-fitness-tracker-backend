b5d973e6f7ec8a61283595091be7bce5d3157465
Aleksandr Vago <vagoalex666@mail.ru>
2024-10-22 23:14:19 +0300

    feat: added base nest.js config

---
+++ src/configs/mongo.config.ts
@@ -0,0 +1,25 @@
+import { ConfigService } from '@nestjs/config';
+import { MongooseModuleOptions } from '@nestjs/mongoose';
+
+export const getMongoConfig = async (
+    configService: ConfigService,
+): Promise<MongooseModuleOptions> => {
+    return {
+        uri: getMongoString(configService),
+        ...getMongoOptions(),
+    };
+};
+
+const getMongoString = (configService: ConfigService) => {
+    const LOGIN = configService.get('MONGO_LOGIN');
+    const PASSWORD = configService.get('MONGO_PASSWORD');
+    const CLUSTER = configService.get('MONGO_CLUSTER');
+    
+    return `mongodb+srv://${LOGIN}:${PASSWORD}@${CLUSTER}.jroy4gr.mongodb.net/`;
+};
+
+const getMongoOptions = () => {
+    return {
+        autoIndex: true,
+    };
+};
